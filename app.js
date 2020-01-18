266da83a6618512f2e003842b408f1422edf34f8
Tyler King <doitliketyler@gmail.com>
2020-01-17 23:05:53 -0800

    Project files

---
+++ app.js
@@ -0,0 +1,101 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const stylus = require('stylus');
+const request = require('request');	
+const Prismic = require('prismic-javascript');
+const PrismicDOM = require('prismic-dom');
+const PrismicConfig = require('./prismic-configuration');
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  process.stdout.write(`Point your browser to: http://localhost:${port}\n`);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: PrismicConfig.apiEndpoint,
+    linkResolver: PrismicConfig.linkResolver,
+  };
+  // add PrismicDOM in locals to access them in templates.
+  res.locals.PrismicDOM = PrismicDOM;
+  Prismic.api(PrismicConfig.apiEndpoint, {
+    accessToken: PrismicConfig.accessToken,
+    req,
+  }).then((api) => {
+    req.prismic = { api };
+    next();
+  }).catch((error) => {
+    next(error.message);
+  });
+});
+
+// Connect to Prismic API
+function initApi(req) {
+  return Prismic.getApi(PrismicConfig.apiEndpoint, {
+    req: req
+  });
+}
+
+// Prismic preview
+app.get('/preview', (req, res) => {
+  const { token } = req.query;
+  if (token) {
+    req.prismic.api.previewSession(token, PrismicConfig.linkResolver, '/').then((url) => {
+      res.redirect(302, url);
+    }).catch((err) => {
+      res.status(500).send(`Error 500 in preview: ${err.message}`);
+    });
+  } else {
+    res.send(400, 'Missing token from querystring');
+  }
+});
+
+// Routes
+app.get('/', function (req, res) {	
+  initApi(req).then(function(api) {
+    api.query(
+      Prismic.Predicates.at('document.type', 'home')
+    ).then(function(response) {
+      res.render('index', 
+        {
+          document: response.results[0].data
+        }
+      );
+    });
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
